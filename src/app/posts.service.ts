e40bad98b5870e5421fcb2c668a547fdeec40c01
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-20 18:01:19 +0200

    headers

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
 
@@ -30,7 +30,11 @@ export class PostsService {
   }
 
   fetchPosts() {
-    return this.http.get<{[key: string]: Post}>('https://ng-complete-guide-bd60b.firebaseio.com/posts.json')
+    return this.http.get<{[key: string]: Post}>('https://ng-complete-guide-bd60b.firebaseio.com/posts.json', {
+      headers: new HttpHeaders({
+        'Custom-Header': 'Hello'
+      })
+    })
       .pipe(map((responseData) => {
         const postsArray: Post[] = [];
 
