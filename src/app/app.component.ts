b9287f90eab94c6273d7a15d69e18ac3bb0d0938
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-20 17:43:22 +0200

    errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ import { PostsService } from './posts.service';
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
+  error = null;
 
   constructor(private http: HttpClient, private postsService: PostsService) {}
 
@@ -22,6 +23,8 @@ export class AppComponent implements OnInit {
     this.postsService.fetchPosts().subscribe((posts) => {
       this.isFetching = false;
       this.loadedPosts = posts;
+    }, (error) => {
+      this.error = error.message;
     });
   }
 
@@ -35,6 +38,9 @@ export class AppComponent implements OnInit {
     this.postsService.fetchPosts().subscribe((posts) => {
       this.isFetching = false;
       this.loadedPosts = posts;
+    }, (error) => {
+      this.error = error.message;
+      console.log(error);
     });
   }
 
