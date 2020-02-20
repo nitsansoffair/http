44f2823a1a20007b2fd8ab5f1313805a346cc1a9
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-20 18:05:08 +0200

    params

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { catchError, map } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
 
@@ -30,10 +30,13 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
+
     return this.http.get<{[key: string]: Post}>('https://ng-complete-guide-bd60b.firebaseio.com/posts.json', {
-      headers: new HttpHeaders({
-        'Custom-Header': 'Hello'
-      })
+      headers: new HttpHeaders({'Custom-Header': 'Hello'}),
+      params: searchParams
     })
       .pipe(map((responseData) => {
         const postsArray: Post[] = [];
